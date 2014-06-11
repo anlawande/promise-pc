0e7699feab438a416eb2a0fcca3cae0880b4dcb3
Aniket Lawande <anlawande@gmail.com>
2014-06-11 21:56:00 +0530

    HTTP consumer test, package.json update for tests

--- tests.js
+++ tests.js
@@ -1,6 +1,7 @@
 var PCQueue = require("./index.js").PCQueue;
 var Promise = require("promise");
 var assert = require("assert");
+var request = require("request");
 
 describe("Simple consumer", function(){
     it("should produce and consume 5 items", function(done) {
@@ -46,4 +47,56 @@ describe("Simple consumer", function(){
             });
         }
     });
+});
+
+describe("HTTP request consumer", function() {
+    it("should fire off 5 HTTP requests, 2 at a time", function(done) {
+        //Create new PCQueue
+        var pcqueue = new PCQueue({
+            maxParallel : 2
+        });
+        //To verify
+        var num_consumed = 0, num_produced = 0;
+        
+        var urls = ["http://www.google.com", "http://www.facebook.com", "http://www.github.com", "http://www.twitter.com", "http://www.stackoverflow.com"];
+
+        //Storing promises to know when we are done with the test
+        var promises = [];
+        for(var i = 0; i < urls.length; i++) {
+            var url = urls[i];
+            //Put something on the queue, passing in the consumer function (MUST return a promise)
+            var promise = pcqueue.produce(function() {
+                return requestConsumer(url);
+            });
+            
+            //Producer is notified when item is consumed
+            promise.done((function(url){    //Stamping the function with url, since it's in a loop
+                return function(data) {
+                    console.log(url + " - status " + data);
+                    num_produced++;
+                }
+            })(url));
+            
+            promises.push(promise);
+        }
+        
+        Promise.all(promises).then(function(){
+            assert.equal(num_consumed, num_produced);
+            done();
+        });
+        
+        //Simple function to return a consumer which waits a random time
+        function requestConsumer(url) {
+            return new Promise(function(resolve, reject){
+                request(url, function(err, response, body) {
+                    if(err) {
+                        reject(err);
+                        return;
+                    }
+                    num_consumed++;
+                    resolve(response.statusCode);
+                });
+            });
+        }
+    })
 });
